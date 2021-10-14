ca5627456e6d1c154418ba9046515846f25997ff
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-10-15 08:43:57 +0900

    Apply Socket.IO

--- src/client/app.js
+++ src/client/app.js
@@ -1,16 +1,14 @@
-// const ws = new WebSocket("ws://localhost:8080/");
-import "/socket.io/socket.io/js";
 const socket = io();
 
 let inputForm = document.querySelector("form")
 let inputTxt = document.getElementById("inputTxt");
 let fromServer = document.getElementById("fromServer");
 
-ws.addEventListener("open", () => {
+socket.addEventListener("open", () => {
     console.log("Client Connected!");
 });
 
-ws.addEventListener("message", (event) => {
+socket.addEventListener("message", (event) => {
     const me = event.data;
     const meLine = document.createElement('div');
     meLine.textContent = me;
@@ -20,6 +18,6 @@ ws.addEventListener("message", (event) => {
 inputForm.addEventListener("submit", (event) => {
     event.preventDefault();
     let message = inputTxt.value;
-    ws.send(message);
+    socket.send(message);
     inputTxt.value = "";
 });
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-import { Server } from "socket.io";
 import { createServer } from "http";
+import { Server } from "socket.io";
 import express from "express";
 import router from "./router";
 import path from "path";
@@ -7,25 +7,14 @@ import path from "path";
 const app = express();
 const port = 8080;
 
+
 app.use("/", router);
 app.set("view engine", "pug");
 app.set("views", path.join(process.cwd(), "/src/views"));
 app.use("/client", express.static(__dirname + "/client"));
 
 const server = createServer(app);
-const io = new Server({ server });
-
-/* const sockets = [];
-
-wss.on("connection", (socket) => { 
-    console.log("webSocket server connected!"); 
-    sockets.push(socket);
-    socket.on("message", (message) => {
-        sockets.forEach(aSocket => {
-            aSocket.send(message.toString('utf8'));
-        });
-    });
-}); */
+const io = new Server(server);
 
 io.on("connection", (socket) => { console.log("io connected!"); });
 server.listen(port, () => { console.log("Express in HTTP connected!") });
