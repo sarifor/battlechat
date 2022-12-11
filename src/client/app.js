31ab55ae9227c8222ff4e69d3ca15dd666140524
sarifor <iread1q84@gmail.com>
2022-12-11 21:56:58 +0900

    Try to send room name and show all room names: cannot read another html page element

--- src/client/app.js
+++ src/client/app.js
@@ -10,53 +10,52 @@ let inputMessage = document.getElementById("inputMessage");
 let leaveForm = document.getElementById("leaveForm");
 let messages = document.getElementById("messages"); */
 
-clientIo.on("connection", () => {
-    let roomName;
+let roomName;
 
-    /* function addMessage (message) {
-        const meLine = document.createElement('div');
-        meLine.textContent = message;
-        messages.append(meLine);
-    }; */
-    
-    inputRoomNameForm.addEventListener("submit", (event) => {
-        event.preventDefault();
-        roomName = inputRoomName.value;
-    
-        clientIo.emit("roomName", roomName);
-        inputRoomName.value = "";
-    
-        // const rooms = document.createElement('div');
-        // rooms.textContent = roomName;
-        // messages.append(rooms);
-    });
-    
-    /* inputMessageForm.addEventListener("submit", (event) => {
-        event.preventDefault();
-        let message = inputMessage.value;
-        inputMessage.value = "";
-    
-        clientIo.emit("messageFromClient", roomName, message);
-        addMessage(message);
-    }); */
-    
-    /* leaveForm.addEventListener("submit", (event) => {
-        event.preventDefault();
-        clientIo.emit("leaveRoom");
-    }); */
-    
-    /* clientIo.on("messageFromClient", (message) => {
-        addMessage(message);
-    }); */
-    
-    clientIo.on("roomName", (roomNames) => {
-        console.log(roomNames);
+/* function addMessage (message) {
+    const meLine = document.createElement('div');
+    meLine.textContent = message;
+    messages.append(meLine);
+}; */
 
-        // roomNames를 for문으로 돌려, 각 방 이름 보이기
-        for (let i = 0; ; i++) {
-            const room = document.createElement('div');
-            room.textContent = roomNames[i];
-            displayRoomNames.append(room);
-        }
-    });
+inputRoomNameForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    roomName = inputRoomName.value;
+
+    clientIo.emit("roomName", roomName);
+    inputRoomName.value = "";
+
+    // const rooms = document.createElement('div');
+    // rooms.textContent = roomName;
+    // messages.append(rooms);
+});
+
+/* inputMessageForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let message = inputMessage.value;
+    inputMessage.value = "";
+
+    clientIo.emit("messageFromClient", roomName, message);
+    addMessage(message);
+}); */
+
+/* leaveForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    clientIo.emit("leaveRoom");
+}); */
+
+/* clientIo.on("messageFromClient", (message) => {
+    addMessage(message);
+}); */
+
+clientIo.on("roomName", (roomNames) => {
+    console.log(roomNames);
+    console.log(displayRoomNames); // null
+
+    // roomNames를 for문으로 돌려, 각 방 이름 보이기
+    for (let i = 0; ; i++) {
+        const room = document.createElement('div');
+        room.textContent = roomNames[i];
+        displayRoomNames.append(room);
+    }
 });
